87e9bb70cb335db4549def12d110dae0fa54bd3e
Mineev_IA <H.E.L.P.M.E@yandex.ru>
2025-12-12 08:36:53 +0500

    Solution to task 273 has successfully the public and my tests

---
+++ src/main/kotlin/task273/Task273.kt
@@ -0,0 +1,63 @@
+package task273
+
+import kotlin.apply
+import kotlin.math.max
+import kotlin.math.min
+
+var answer: String = ""
+    private set
+
+fun main() {
+    val input = readInput()
+    start(input)
+}
+
+fun start(input: Input) {
+    answer = getAnswer(solution(input))
+    println(answer)
+}
+
+// Если разбирать значения прямо во время ввода,
+// то можно ускорить решение задачи.
+// Но для тестов мне удобнее так
+private fun readInput(): Input {
+    val (s, n) = readln().split(" ").map { it.toInt() }
+    return Input(s, n).apply {
+        (1..this.n).forEach {
+            coordinates += readln().toInt()
+        }
+    }
+}
+
+private fun solution(input: Input): Int {
+    // Расстояние до самого дальнего дома слева
+    var left = 0
+
+    // Расстояние до самого дальнего дома справа
+    var right = 0
+
+    input.coordinates.forEach { house ->
+        val distance: Int
+        when {
+            house < input.s -> {
+                distance = input.s - house
+                if (left < distance) left = distance
+            }
+            house > input.s -> {
+                distance = house - input.s
+                if (right < distance) right = distance
+            }
+        }
+    }
+
+    val minDistance = min(left, right) * 2 + max(left, right)
+    return minDistance
+}
+
+private fun getAnswer(answer: Int): String {
+    return answer.toString()
+}
+
+data class Input(val s: Int, val n: Int) {
+    val coordinates = mutableListOf<Int>()
+}
---
+++ src/main/kotlin/task273/Test273.kt
@@ -0,0 +1,68 @@
+package task273
+
+import kotlin.math.pow
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
+
+private val tests = listOf(
+    // Public tests
+    Test(
+        "Test 1",
+        Input(25270880, 1).apply { coordinates.addAll(listOf(-48940870)) },
+        "74211750"),
+
+    // My test
+    Test(
+        title = "My test 1",
+        input = Input(1, 3).apply { coordinates.addAll(listOf(-3, 2, -1)) },
+        answer = "6"
+    ),
+    Test(
+        title = "My test 2",
+        input = Input(0, 4).apply { coordinates.addAll(listOf(10, -3, -10, 3)) },
+        answer = "30"
+    ),
+    Test(
+        title = "My test 2",
+        input = Input(-15, 4).apply { coordinates.addAll(listOf(10, -3, -10, 3)) },
+        answer = "25"
+    ),
+    Test(
+        title = "My test 2",
+        input = Input(-15, 4).apply { coordinates.addAll(listOf(-20, 10, -3, -10, 3)) },
+        answer = "35"
+    ),
+    createLongTest()
+)
+
+fun main() {
+
+    tests.forEach { startWithTime(it) }
+
+}
+
+private fun startWithTime(test: Test) {
+    val time = measureTimeMillis { start(test.input) }
+    println("answer is ${if (answer == test.answer) "correct" else "incorrect"}!")
+    println("${test.title}: lead time = $time ms")
+}
+
+private fun createLongTest(): Test {
+    val maxDistance = (10.0.pow(8)).toInt()
+    val left = -maxDistance
+    val right = maxDistance
+    val s = Random.nextInt(left, right)
+    val n = 100000
+    val houses = List(n) { Random.nextInt(left, right) }
+    return Test(
+        "LONG test",
+        Input(s, n).apply { coordinates.addAll(houses) },
+        "0"
+    )
+}
+
+data class Test(
+    val title: String,
+    val input: Input,
+    val answer: String
+)
